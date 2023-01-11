f5a8393bf326a88fb4e6789dd6360bf2e66309ae
Amin <aminkhalif99@gmail.com>
2023-01-11 12:44:52 -0500

    removed powershell test

--- tests/example.test.js
+++ tests/example.test.js
@@ -1,6 +1,10 @@
-const cloudinaryConfig = require('./cloudinary.config');
-
-test('cloudinary is properly imported and initialized', () => {
-    const cloudinary = require('cloudinary');
-    expect(cloudinary.v2).toBeDefined();
+test('cloudinary can upload', async () => {
+  const cloudinary = require('cloudinary');
+  cloudinary.config({
+    cloud_name: 'YOUR_CLOUD_NAME',
+    api_key: 'YOUR_API_KEY',
+    api_secret: 'YOUR_API_SECRET'
   });
+  const result = await cloudinary.v2.uploader.upload('image.png', { public_id: 'image_id' });
+  expect(result).toHaveProperty('public_id');
+});
